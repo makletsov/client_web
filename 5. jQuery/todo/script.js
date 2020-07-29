f6bf1ff3ac09a97c738c05302714df4a20e486fa
MakletsovVE <v.makletsov@yandex.ru>
2020-07-29 13:37:18 +0700

    Todo. Last issues have been fixed.

--- 5. jQuery/todo/script.js
+++ 5. jQuery/todo/script.js
@@ -1,52 +1,68 @@
-(function ($, undefined) {
-    $(function () {
-        var $inputTextField = $("#note-input-field");
+$(function () {
+    var $inputTextField = $("#note-input-field");
 
-        $("#note-add-button").click(function () {
-            var inputText = $inputTextField.val();
+    $("#note-add-button").click(function () {
+        var inputText = $inputTextField.val();
 
-            $inputTextField.val("");
-            $inputTextField.focus();
+        $inputTextField.val("");
+        $inputTextField.focus();
 
-            var $emptyFieldAlert = $("#empty-field-alert");
+        var $emptyFieldAlert = $("#empty-field-alert");
 
-            if (!inputText.trim()) {
-                $emptyFieldAlert.text("Cannot add an empty note. Insert note text in a text field.");
-                return;
-            }
+        function showEmptyFieldAlert() {
+            $emptyFieldAlert.text("Cannot add an empty note. Insert note text in a text field.");
+        }
 
+        function hideEmptyFieldAlert() {
             $emptyFieldAlert.text("");
+        }
 
-            var $notesListItem = $('<li class="list-item"></li>');
+        if (!inputText.trim()) {
+            showEmptyFieldAlert();
+            return;
+        }
 
-            function switchToViewMode() {
-                $notesListItem.html('<span class="note-text-label"></span><button class="edit-button">Edit</button><button class="delete-button">Delete</button>');
-                $notesListItem.find(".note-text-label").text(inputText);
+        hideEmptyFieldAlert();
 
-                $notesListItem.find(".edit-button").click(function () {
-                    $notesListItem.html('<input type="text" class="edit-text-field"><button class="apply-button">Apply</button><button class="cancel-button">Cancel</button>');
-                    var $editTextField = $notesListItem.find(".edit-text-field");
-                    $editTextField.val(inputText).select();
+        var $notesListItem = $('<li class="list-item"></li>');
 
-                    $notesListItem.find(".apply-button").click(function () {
+        function switchToViewMode() {
+            $notesListItem.html('<span class="note-text-label"></span><button class="edit-button">Edit</button><button class="delete-button">Delete</button>');
+
+            $notesListItem.find(".note-text-label").text(inputText);
+
+            $notesListItem.find(".edit-button").click(function () {
+                $notesListItem.html('<input type="text" class="edit-text-field"><button class="apply-button">Apply</button><button class="cancel-button">Cancel</button>');
+
+                var $editTextField = $notesListItem.find(".edit-text-field");
+                $editTextField.val(inputText).select();
+
+                $notesListItem.find(".apply-button").click(function () {
+                    if ($editTextField.val()) {
+                        hideEmptyFieldAlert();
                         inputText = $editTextField.val();
                         switchToViewMode();
-                    });
-
-                    $notesListItem.find(".cancel-button").click(function () {
-                        switchToViewMode();
-                    });
+                    } else {
+                        $editTextField.focus();
+                        showEmptyFieldAlert();
+                    }
                 });
 
-                $notesListItem.find(".delete-button").click(function () {
-                    $notesListItem.remove();
+                $notesListItem.find(".cancel-button").click(function () {
+                    switchToViewMode();
                 });
-            }
+            });
 
-            switchToViewMode();
+            $notesListItem.find(".delete-button").click(function () {
+                $notesListItem.remove();
+                $inputTextField.focus();
+            });
 
-            var $notesList = $("#notes-list");
-            $notesList.append($notesListItem);
-        });
+            $inputTextField.focus();
+        }
+
+        switchToViewMode();
+
+        $("#notes-list").append($notesListItem);
     });
-})(jQuery);
+});
