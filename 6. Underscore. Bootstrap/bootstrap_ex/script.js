a8699fdcb8dad56f980fe71fb923790337156a76
MakletsovVE <v.makletsov@yandex.ru>
2020-07-30 22:47:36 +0700

    Bootstarp_ex. Version 2 done.

--- 6. Underscore. Bootstrap/bootstrap_ex/script.js
+++ 6. Underscore. Bootstrap/bootstrap_ex/script.js
@@ -1,31 +1,40 @@
 $(function () {
+    function hideEditDeleteButtonGroups() {
+        $('.cancel-button').click();
+        $('.edit-delete-button-group').css('display', 'none');
+    }
+
     var $inputTextField = $("#note-input-field");
 
-    $("#note-add-button").click(function () {
+    $inputTextField.keyup(function () {
         var inputText = $inputTextField.val();
 
-        $inputTextField.val("");
+        if (inputText.trim()) {
+            $inputTextField.removeClass('is-invalid');
+        }
+    });
+
+    $inputTextField.click(function () {
+        hideEditDeleteButtonGroups();
+    })
+
+    $("#note-add-button").click(function () {
+        $inputTextField.removeClass('is-invalid');
+        $('.cancel-button').click();
+
+        var inputText = $inputTextField.val();
+
         $inputTextField.focus();
 
-        var $emptyFieldAlert = $("#empty-field-alert");
-
-        function showEmptyFieldAlert() {
-            $emptyFieldAlert.text("Cannot add an empty note. Insert note text in a text field.");
-        }
-
-        function hideEmptyFieldAlert() {
-            $emptyFieldAlert.text("");
-        }
-
         if (!inputText.trim()) {
-            showEmptyFieldAlert();
+            $inputTextField.addClass('is-invalid');
             return;
         }
 
-        hideEmptyFieldAlert();
+        $inputTextField.val("");
 
         var $notesListItem = $('<li></li>')
-            .addClass("list-group-item row justify-content-center")
+            .addClass("list-item list-group-item row justify-content-center")
             .append($('<div>')
                 .addClass("note-text-label"))
             .append($('<div>')
@@ -40,8 +49,9 @@ $(function () {
         var initialNoteItemHtml = $notesListItem.html();
 
         $notesListItem.click(function () {
-            $('.cancel-button').click();
-            $('.edit-delete-button-group').css('display', 'none');
+            $inputTextField.removeClass('is-invalid');
+
+            hideEditDeleteButtonGroups();
 
             $notesListItem.find('.edit-delete-button-group').css('display', 'inline-flex');
         });
@@ -49,9 +59,7 @@ $(function () {
         function switchToViewMode() {
             $notesListItem.html(initialNoteItemHtml);
 
-            $notesListItem
-                .find('.note-text-label')
-                .text(inputText);
+            $notesListItem.find('.note-text-label').text(inputText);
 
             $notesListItem.find(".edit-button").click(function (event) {
                 event.stopPropagation();
@@ -62,7 +70,11 @@ $(function () {
 
                 $notesListItem
                     .append($('<textarea>')
-                        .addClass("edit-text-field col-12"))
+                        .addClass("edit-text-field form-control col-12"))
+                    .append($('<div>')
+                        .text('Cannot save an empty note!')
+                        .prop('id', 'edit-field-alert')
+                        .addClass("invalid-feedback text-center col-12"))
                     .append($('<div>')
                         .addClass('apply-cancel-button-group btn-group mt-2')
                         .append($('<button>')
@@ -75,17 +87,22 @@ $(function () {
                 var $editTextField = $notesListItem.find(".edit-text-field");
                 $editTextField.val(inputText).select();
 
+                $editTextField.keyup(function () {
+                    if ($editTextField.val()) {
+                        $editTextField.removeClass('is-invalid');
+                    }
+                })
+
                 $notesListItem.find(".apply-button").click(function (event) {
                     event.stopPropagation();
+                    $editTextField.removeClass('is-invalid');
 
                     if ($editTextField.val()) {
-                        hideEmptyFieldAlert();
                         inputText = $editTextField.val();
                         switchToViewMode();
                     } else {
-                        /*debugger;*/
+                        $editTextField.addClass('is-invalid');
                         $editTextField.focus();
-                        showEmptyFieldAlert();
                     }
                 });
 
