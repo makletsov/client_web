64984983dc613bb67a57432fa5c59244e199575c
MakletsovVE <v.makletsov@yandex.ru>
2020-08-05 17:46:13 +0700

    Bootstrap_ex. Issues fixed.

--- 6. Underscore. Bootstrap/bootstrap_ex/script.js
+++ 6. Underscore. Bootstrap/bootstrap_ex/script.js
@@ -1,59 +1,64 @@
 $(function () {
-    function hideEditDeleteButtonGroups() {
-        $('.cancel-button').click();
-        $('.edit-delete-button-group').css('display', 'none');
+    function switchAllItemsToLeanModeExceptGiven(item) {
+        $('.list-item')
+            .not(item)
+            .find('.cancel-button')
+            .click();
+
+        $('.edit-delete-button-group').hide();
     }
 
-    var $inputTextField = $("#note-input-field");
+    var $noteInputField = $('.note-input-field');
 
-    $inputTextField.keyup(function () {
-        var inputText = $inputTextField.val();
+    $noteInputField.keyup(function () {
+        var inputText = $noteInputField.val();
 
         if (inputText.trim()) {
-            $inputTextField.removeClass('is-invalid');
+            $noteInputField.removeClass('is-invalid');
         }
     });
 
-    $inputTextField.click(function () {
-        hideEditDeleteButtonGroups();
-    })
+    $noteInputField.click(function () {
+        switchAllItemsToLeanModeExceptGiven();
+    });
 
-    $("#note-add-button").click(function () {
-        $inputTextField.removeClass('is-invalid');
+    $('.note-add-button').click(function () {
+        $noteInputField.removeClass('is-invalid');
         $('.cancel-button').click();
 
-        var inputText = $inputTextField.val();
+        var inputText = $noteInputField.val();
 
-        $inputTextField.focus();
+        $noteInputField.focus();
 
         if (!inputText.trim()) {
-            $inputTextField.addClass('is-invalid');
+            $noteInputField.addClass('is-invalid');
             return;
         }
 
-        $inputTextField.val("");
+        $noteInputField.val('');
 
         var $notesListItem = $('<li></li>')
-            .addClass("list-item list-group-item row justify-content-center")
+            .addClass('list-item list-group-item row justify-content-center')
             .append($('<div>')
-                .addClass("note-text-label"))
+                .addClass('note-text-label col-12'))
             .append($('<div>')
-                .addClass('edit-delete-button-group btn-group mt-2')
+                .addClass('edit-delete-button-group btn-group mt-2 col-12')
+                .hide()
                 .append($('<button>')
-                    .text("Edit")
-                    .addClass("edit-button btn btn-dark"))
+                    .text('Edit')
+                    .addClass('edit-button btn btn-dark'))
                 .append($('<button>')
-                    .text("Delete")
-                    .addClass("delete-button btn btn-danger")));
+                    .text('Delete')
+                    .addClass('delete-button btn btn-danger')));
 
         var initialNoteItemHtml = $notesListItem.html();
 
         $notesListItem.click(function () {
-            $inputTextField.removeClass('is-invalid');
+            $noteInputField.removeClass('is-invalid');
 
-            hideEditDeleteButtonGroups();
+            switchAllItemsToLeanModeExceptGiven($notesListItem);
 
-            $notesListItem.find('.edit-delete-button-group').css('display', 'inline-flex');
+            $notesListItem.find('.edit-delete-button-group').show();
         });
 
         function switchToViewMode() {
@@ -61,7 +66,7 @@ $(function () {
 
             $notesListItem.find('.note-text-label').text(inputText);
 
-            $notesListItem.find(".edit-button").click(function (event) {
+            $notesListItem.find('.edit-button').click(function (event) {
                 event.stopPropagation();
 
                 $('.cancel-button').click();
@@ -70,30 +75,29 @@ $(function () {
 
                 $notesListItem
                     .append($('<textarea>')
-                        .addClass("edit-text-field form-control col-12"))
+                        .addClass('edit-text-field form-control col-12'))
                     .append($('<div>')
                         .text('Cannot save an empty note!')
-                        .prop('id', 'edit-field-alert')
-                        .addClass("invalid-feedback text-center col-12"))
+                        .addClass('edit-field-alert invalid-feedback text-center col-12'))
                     .append($('<div>')
-                        .addClass('apply-cancel-button-group btn-group mt-2')
+                        .addClass('apply-cancel-button-group btn-group col-12 mt-2')
                         .append($('<button>')
-                            .text("Apply")
-                            .addClass("apply-button btn btn-dark"))
+                            .text('Apply')
+                            .addClass('apply-button btn btn-dark'))
                         .append($('<button>')
-                            .text("Cancel")
-                            .addClass("cancel-button btn btn-danger")));
+                            .text('Cancel')
+                            .addClass('cancel-button btn btn-danger')));
 
-                var $editTextField = $notesListItem.find(".edit-text-field");
+                var $editTextField = $notesListItem.find('.edit-text-field');
                 $editTextField.val(inputText).select();
 
                 $editTextField.keyup(function () {
                     if ($editTextField.val()) {
                         $editTextField.removeClass('is-invalid');
                     }
-                })
+                });
 
-                $notesListItem.find(".apply-button").click(function (event) {
+                $notesListItem.find('.apply-button').click(function (event) {
                     event.stopPropagation();
                     $editTextField.removeClass('is-invalid');
 
@@ -106,22 +110,50 @@ $(function () {
                     }
                 });
 
-                $notesListItem.find(".cancel-button").click(function (event) {
+                $notesListItem.find('.cancel-button').click(function (event) {
                     event.stopPropagation();
                     switchToViewMode();
                 });
             });
 
-            $notesListItem.find(".delete-button").click(function () {
-                $notesListItem.remove();
-                $inputTextField.focus();
+            var $deleteButton = $notesListItem.find('.delete-button');
+
+            $deleteButton.confirmation({
+                rootSelector: '.delete-button',
+                singleton: true,
+                placement: 'bottom',
+                title: 'Are you shure?',
+                buttons: [
+                    {
+                        class: 'btn btn-danger',
+                        iconClass: 'material-icons',
+                        iconContent: 'done_outline',
+                        label: 'Yes'
+                    },
+                    {
+                        class: 'btn btn-dark',
+                        iconClass: 'material-icons',
+                        iconContent: 'cancel',
+                        label: 'No',
+                        cancel: true
+                    }
+                ]
             });
 
-            $inputTextField.focus();
+            $deleteButton.click(function () {
+                $notesListItem.remove();
+                $noteInputField.focus();
+            });
+
+            $noteInputField.focus();
+
+            $('html').click(function () {
+                $deleteButton.confirmation('hide');
+            });
         }
 
         switchToViewMode();
 
-        $("#notes-list").append($notesListItem);
+        $('.notes-list').append($notesListItem);
     });
 });
