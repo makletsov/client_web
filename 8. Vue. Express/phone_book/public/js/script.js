3bf321e92c76b11ae7680c56426eb652589dbfd5
Vasily Makletsov <v.makletsov@yandex.ru>
2020-08-07 15:54:34 +0700

    PhoneBook. Made client side validation.

--- 8. Vue. Express/phone_book/public/js/script.js
+++ 8. Vue. Express/phone_book/public/js/script.js
@@ -17,9 +17,32 @@ var vm = new Vue({
     el: '#app',
     data: {
         contacts: [],
-        firstName: '',
-        lastName: '',
-        phone: '',
+        contact: {
+            newFirstName: {
+                value: '',
+                error: '',
+                validation: {
+                    isValid: false,
+                    isInvalid: false
+                }
+            },
+            newLastName: {
+                value: '',
+                error: '',
+                validation: {
+                    isValid: false,
+                    isInvalid: false
+                }
+            },
+            newPhone: {
+                value: '',
+                error: '',
+                validation: {
+                    isValid: false,
+                    isInvalid: false
+                }
+            }
+        },
         term: ''
     },
     created: function () {
@@ -38,21 +61,78 @@ var vm = new Vue({
             });
         },
         addContact: function () {
+            function checkPropertyText(property, regexp) {
+                var check = property.match(regexp);
+
+                return check && check.length === 1 && property === check[0];
+            }
+
+            function normalisePhoneNumber(phone) {
+                return phone.replace(/[()\- ]/g, "");
+            }
+
+            function validateProperty(property, regexp, propertyString) {
+                if (!property.value) {
+                    property.error = 'Insert ' + propertyString + '!';
+                    property.validation.isValid = false;
+                    property.validation.isInvalid = true;
+                } else if (!checkPropertyText(property.value, regexp)) {
+                    property.error = 'Insert correct ' + propertyString + '!';
+                    property.validation.isValid = false;
+                    property.validation.isInvalid = true;
+                } else {
+                    property.error = '';
+                    property.validation.isValid = true;
+                    property.validation.isInvalid = false;
+                }
+            }
+
+            for (var property in this.contact) {
+                if (this.contact.hasOwnProperty(property)) {
+                    this.contact[property].value = this.contact[property].value.trim();
+                }
+            }
+
+            var nameRegExp = /[a-zA-Zа-яА-ЯёЁ]+[-?a-zA-Zа-яА-ЯёЁ]*/g;
+
+            validateProperty(this.contact.newFirstName, nameRegExp, 'first name');
+            validateProperty(this.contact.newLastName, nameRegExp, 'last name');
+
+            var phoneRegExp = /^\+?[0-9]+/g;
+
+            this.contact.newPhone.value = normalisePhoneNumber(this.contact.newPhone.value);
+
+            validateProperty(this.contact.newPhone, phoneRegExp, 'phone number');
+
+            if (this.contact.newFirstName.validation.isInvalid
+                || this.contact.newLastName.validation.isInvalid
+                || this.contact.newPhone.validation.isInvalid) {
+                return;
+            }
+
             var self = this;
 
             post('contacts/add', {
                 contact: {
-                    firstName: this.firstName,
-                    lastName: this.lastName,
-                    phone: this.phone
+                    firstName: this.contact.newFirstName.value,
+                    lastName: this.contact.newLastName.value,
+                    phone: this.contact.newPhone.value
                 }
-            }).done(function (data) {
+            }).done(function () {
                 self.loadContacts();
             }).fail(function (data) {
                 if (!data.responseJSON.success) {
                     alert(data.responseJSON.message);
                 }
             });
+
+            for (property in this.contact) {
+                if (this.contact.hasOwnProperty(property)) {
+                    this.contact[property].value = '';
+                    this.contact[property].validation.isInvalid = false;
+                    this.contact[property].validation.isValid = false;
+                }
+            }
         },
         deleteContact: function (contact) {
             var self = this;
--- 8. Vue. Express/phone_book/routes/index.js
+++ 8. Vue. Express/phone_book/routes/index.js
@@ -10,7 +10,8 @@ router.get('/contacts', function(req, res) {
   res.send(term.length === 0
       ? contacts
       : contacts.filter(function (contact) {
-        return contact.name.toUpperCase().indexOf(term) >= 0 || contact.phone.toUpperCase().indexOf(term) >= 0;
+        return contact.name.toUpperCase().indexOf(term) >= 0
+            || contact.phone.toUpperCase().indexOf(term) >= 0;
       }));
 });
 
@@ -37,10 +38,12 @@ router.post('/contacts/delete', function (req, res) {
 });
 
 router.post('/contacts/add', function (req, res) {
-  function sendErrorRequest(status, message) {
+  var messages = [];
+
+  function sendErrorRequest(status) {
     res.status(status).send({
       success: false,
-      message: message
+      message: messages
     });
   }
 
@@ -50,54 +53,50 @@ router.post('/contacts/add', function (req, res) {
     return check && check.length === 1 && property === check[0];
   }
 
-  function normalisePhoneNumber() {
-    return this.replace(/[()\- ]/g, "");
+  function normalisePhoneNumber(phone) {
+    return phone.replace(/[()\- ]/g, "");
   }
 
   var contact = req.body.contact;
 
   if (!contact) {
-    sendErrorRequest(400,'Contact to add should be transferred in a request body!');
+    messages.push('Contact to add should be transferred in the request body!');
+    sendErrorRequest(400);
     return;
   }
 
   for (var property in contact) {
-    property = property.trim();
+    if (contact.hasOwnProperty(property)) {
+      property = property.trim();
+    }
   }
 
-  contact.phone = normalisePhoneNumber.call(contact.phone);
-
-  if (!contact.firstName) {
-    sendErrorRequest(400,'Contact to add should contain the first name!');
-    return;
-  }
-
-  if (!contact.lastName) {
-    sendErrorRequest(400,'Contact to add should contain the last name!');
-    return;
-  }
-
-  if (!contact.phone) {
-    sendErrorRequest(400,'Contact to add should contain a phone number!');
-    return;
-  }
+  contact.phone = normalisePhoneNumber(contact.phone);
 
   var nameRegExp = /[a-zA-Zа-яА-ЯёЁ]+[-?a-zA-Zа-яА-ЯёЁ]*/g;
 
-  if (!checkProperty(contact.firstName, nameRegExp)) {
-    sendErrorRequest(400,'Contact to add has an incorrect first name!');
-    return;
+  if (!contact.firstName) {
+    messages.push('Contact to add should contain the first name!');
+  } else if (!checkProperty(contact.firstName, nameRegExp)) {
+    messages.push('Contact to add has an incorrect first name!');
   }
 
-  if (!checkProperty(contact.lastName, nameRegExp)) {
-    sendErrorRequest(400,'Contact to add has an incorrect last name!');
-    return;
+  if (!contact.lastName) {
+    messages.push('Contact to add should contain the last name!');
+  } else if (!checkProperty(contact.lastName, nameRegExp)) {
+    messages.push('Contact to add has an incorrect last name!');
   }
 
   var phoneRegExp = /^\+?[0-9]+/g;
 
-  if (!checkProperty(contact.phone, phoneRegExp)) {
-    sendErrorRequest(400,'Contact to add has an incorrect phone number!');
+  if (!contact.phone) {
+    messages.push('Contact to add should contain a phone number!');
+  } else if (!checkProperty(contact.phone, phoneRegExp)) {
+    messages.push('Contact to add has an incorrect phone number!');
+  }
+
+  if (messages.length) {
+    sendErrorRequest(400);
     return;
   }
 
@@ -106,7 +105,8 @@ router.post('/contacts/add', function (req, res) {
   });
 
   if (isRepeat) {
-    sendErrorRequest(409, 'The contact with the same phone number is already exist in the phone book!');
+    messages.push('The contact with the same phone number is already exist in the phone book!');
+    sendErrorRequest(409);
     return;
   }
 
